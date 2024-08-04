ea5a05bf85b46ee2b401bafce7902ccc05bb8f49
Dmitry <79802366+clienthold@users.noreply.github.com>
2024-08-04 13:15:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 HONEYPOT_LIST = ["chrome.exe", "firefox.exe"] # LIST OF FILES
-PAYLOAD_PATH = "payload.exe" # PATH FOR PAYLOAD FILE
+PAYLOAD_PATH = "" # PATH FOR PAYLOAD FILE
 LOG_FILE = "honeypot.log" # PATH FOR LOG FILE
